037aa591c5cc3dc4ca61d105d9fb83da86ae9849
Mariana Silva <108891919+umanormari@users.noreply.github.com>
2023-10-31 19:53:20 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import pyautogui
+from time import sleep
+
+# NO PROMPT DE COMANDO DO PC, DIGITE:
+# >>> from mouseinfo import mouseInfo
+# >>> mouseInfo()
+
+# 1- CLICAR E DIGITAR MEU USUARIO
+pyautogui.click()
+pyautogui.write('Mariana')
+# 2- CLICAR E DIGITAR MINHA SENHA
+pyautogui.click()
+pyautogui.write('123456')
+
+# 3- CLICAR EM "ENTRAR"
+pyautogui.click()
+
+# 4- EXTRAIR CADA PRODUTO
+with open('produtos.txt', 'r') as arquivo:
+    for linha in arquivo:
+        produto = linha.split(',')[0]
+        quantidade = linha.split(',')[1]
+        preco = linha.split(',')[2]
+        # 1- Clicar e digitar o produto
+        pyautogui.click()
+        pyautogui.write(produto)
+        # 2- Clicar e registrar a quantidade
+        pyautogui.click()
+        pyautogui.write(quantidade)
+        # 3- Clicar e registrar o preço
+        pyautogui.click()
+        pyautogui.write(preco)
+        # 4- Clicar em registrar tudo
